d18a551a0654b6dd3a15f5b8da5566cc4b9940a6
Sudharsan Sukumar <sudharsan.sukumar5@gmail.com>
2017-10-18 02:10:33 +0100

    Add file listDir command

--- src/services/device/file.service.ts
+++ src/services/device/file.service.ts
@@ -31,13 +31,52 @@ export class FileService {
         };
         return Observable.create((observer) => {
             let dataToSend: Uint8Array = this.commandUtilService.createChunkedArrayBuffer(command, file);
-            console.log(String.fromCharCode.apply(null, dataToSend));
             this.transport.writeRead('/', dataToSend, 'binary').subscribe(
                 (arrayBuffer) => {
                     let data;
                     try {
                         let stringify = String.fromCharCode.apply(null, new Uint8Array(arrayBuffer.slice(0)));
-                        console.log(stringify);
+                        data = JSON.parse(stringify);
+                    }
+                    catch (e) {
+                        observer.error(e);
+                        return;
+                    }
+                    if (data == undefined || data.agent != undefined) {
+                        observer.error(data);
+                        return;
+                    }
+                    if (data.file == undefined || data.file[0] == undefined || data.file[0].statusCode !== 0) {
+                        observer.error(data);
+                        return;
+                    }
+                    observer.next(data);
+                    observer.complete();
+                },
+                (err) => {
+                    observer.error(err);
+                },
+                () => {
+                    observer.complete();
+                }
+            );
+        });
+    }
+
+    listDir(location: string, path: string): Observable<any> {
+        let command = {
+            file: [{
+                command: 'listdir',
+                type: location,
+                path: path
+            }]
+        };
+        return Observable.create((observer) => {
+            this.transport.writeRead('/', JSON.stringify(command), 'json').subscribe(
+                (arrayBuffer) => {
+                    let data;
+                    try {
+                        let stringify = String.fromCharCode.apply(null, new Uint8Array(arrayBuffer.slice(0)));
                         data = JSON.parse(stringify);
                     }
                     catch (e) {
@@ -79,28 +118,8 @@ export class FileService {
         return Observable.create((observer) => {
             this.transport.writeRead('/', JSON.stringify(command), 'json').subscribe(
                 (arrayBuffer) => {
-                    //Handle device errors and warnings
-                    /*let data;
-                    try {
-                        data = JSON.parse(String.fromCharCode.apply(null, new Int8Array(arrayBuffer.slice(0))));
-                    }
-                    catch (e) {
-                        observer.error(e);
-                        return;
-                    }
-
-                    for (let i = 0; i < chans.length; i++) {
-                        if (data.gpio == undefined || data.gpio[chans[i]][0].statusCode > 0 || data.agent != undefined) {
-                            observer.error(data);
-                            return;
-                        }
-                    }
-                    observer.next(data);
-                    observer.complete();*/
-                    console.log(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer)));
                     this.commandUtilService.observableParseChunkedTransfer(arrayBuffer, 'u8').subscribe(
                         (data) => {
-                            console.log(data);
                             let jsonObject = data.json;
                             let binaryData: Uint8Array = data.typedArray;
                             if (data == undefined || data.agent != undefined) {
@@ -131,46 +150,6 @@ export class FileService {
                     observer.complete();
                 }
             );
-        });
-    }
-
-    private parseJsonResponse(commandObject: Object): Observable<any> {
-        return Observable.create((observer) => {
-            this.transport.writeRead('/', JSON.stringify(commandObject), 'json').subscribe(
-                (arrayBuffer) => {
-                    let data;
-                    try {
-                        let stringify = String.fromCharCode.apply(null, new Int8Array(arrayBuffer.slice(0)));
-                        console.log(stringify);
-                        data = JSON.parse(stringify);
-                    }
-                    catch (e) {
-                        observer.error(e);
-                        return;
-                    }
-                    if (data == undefined || data.agent != undefined) {
-                        observer.error(data);
-                        return;
-                    }
-                    for (let instrument in data) {
-                        for (let channel in data[instrument]) {
-                            if (data[instrument][channel][0].statusCode > 0) {
-                                observer.error(data);
-                                return;
-                            }
-                        }
-                    }
-                    observer.next(data);
-                    //Handle device errors and warnings
-                    observer.complete();
-                },
-                (err) => {
-                    observer.error(err);
-                },
-                () => {
-                    observer.complete();
-                }
-            )
         });
     }
 
